b1dfe6f3f1fbf9da9550cd84530b7e40be0d83d4
agunny <>
2023-10-16 17:50:16 +0000

    added code to return the value of the item depending on the date and item code

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,10 +14,32 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('LPFMWFY23')
+spreadsheet = GSPREAD_CLIENT.open('LPFMWFY23')
 
-sales = SHEET.worksheet('LPF')
 
-data = sales.get_all_values()
+lpf_sheet = spreadsheet.worksheet('LPF')
+approved_sheet = spreadsheet.worksheet('Approved')
+rejected_sheet = spreadsheet.worksheet('Rejected')
 
-print(data)
+
+
+def query_item_price(invoice_date, item_code, site):
+    formatted_date = datetime.strptime(invoice_date, '%d/%m/%Y').strftime('%b-%y')
+    data = lpf_sheet.get_all_records()
+
+    item_found = False
+    for entry in data:
+        if entry['ItemCode'] == item_code and entry['Site'] == site:
+            price = entry.get(formatted_date)
+            if price:
+                item_found = True
+                return price
+    if not item_found:
+        return "Item not found in LPF file, please check the PO/Invoice for the item code again"
+    return "Error in LPF sheet"
+
+invoice_date = "15/11/2023"
+item_code = "P34309"
+site = "MANTON WOOD"
+price = query_item_price(invoice_date, item_code, site)
+print(f"Price for Item Code {item_code} on {invoice_date}: {price}")
