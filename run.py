6cb73a7bced57ff182ca89cbaf5b40c9b14c7500
agunny <>
2023-10-16 20:53:38 +0000

    added LPF price to pull through when running

--- run.py
+++ run.py
@@ -25,65 +25,76 @@ def query_item_price_and_buyer(invoice_date, item_code, site):
 
     item_found = False
     buyer = None
+    price_from_lpf = None
+    
     for entry in data:
         if entry['ItemCode'] == item_code and entry['Site'] == site:
             price = entry.get(formatted_date)
+            price_from_lpf = price
             buyer = entry.get('Buyer')
             if price:
                 item_found = True
-                return price, buyer
+                return price, buyer, price_from_lpf
     if not item_found:
-        return None, None
+        return None, None, None
 
-    return None, None
+    return None, None, None
 
 
 # Function to push Rejected and Approved invoices as per the report status
-def push_to_approved_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer):
+def push_to_approved_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer, price_from_lpf):
     status = "Approved - please pay"
-    row = [invoice_date, document_reference, item_code, site, invoice_price, system_price, buyer, status]
+    row = [invoice_date, document_reference, item_code, site, invoice_price, system_price, buyer, price_from_lpf, status]
     approved_sheet.append_rows([row])
 
-def push_to_rejected_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer):
+def push_to_rejected_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer, price_from_lpf):
     status = "Rejected - please request credit from the supplier"
-    row = [invoice_date, document_reference, item_code, site, invoice_price, system_price, buyer, status]
+    row = [invoice_date, document_reference, item_code, site, invoice_price, system_price, buyer, price_from_lpf, status]
     rejected_sheet.append_rows([row])
 
 # Function to create sheet with either rejected or approved price differences, 1% difference allowed
 def create_rejected_invoices_report():
-    print("\n=== Enter Invoice Details ===")
-    invoice_date = input("Enter invoice date (dd/mm/yyyy: ")
-    item_code = input("Enter item code: ").upper()
-    site = input("Enter site: ").upper()
-    invoice_price = input("Enter invoice price: ")
-    system_price = input("Enter system price: ")
-    document_reference = input("Enter document reference: ")
+    while True:
+        print("\n=== Enter Invoice Details ===")
+        invoice_date = input("Enter invoice date (dd/mm/yyyy): ")
+        document_reference = input("Enter document reference: ")
+        item_code = input("Enter item code: ").upper()
+        site_input = input("Enter site: ").upper()
+        site = "MW" if site_input == "MW" else "MANTON WOOD"        
+        invoice_price = input("Enter invoice price: ")
+        system_price = input("Enter system price: ")
+        
+        price, buyer, price_from_lpf = query_item_price_and_buyer(invoice_date, item_code, site)
 
-    price, buyer = query_item_price_and_buyer(invoice_date, item_code, site)
-    
-    print("\n=== Report Results ===")
-    if price is not None:
-        if abs(float(invoice_price) - float(price)) <= 0.01:
-            push_to_approved_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer)
-            print("Invoice Approved. Details:")
-            print("Invoice Date:", invoice_date)
-            print("Item Code:", item_code)
-            print("Site:", site)
-            print("Invoice Price", invoice_price)
-            print("System Price", system_price)
-            print("Document Reference:", document_reference)
-            print("Buyer", buyer)
+        print("\n=== Report Results ===")
+        if price is not None:
+            if abs(float(invoice_price) - float(price)) <= 0.01:
+                push_to_approved_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer, price_from_lpf)
+                print("Invoice Approved.")
+                print("Invoice Date:", invoice_date)
+                print("Item Code:", item_code)
+                print("Site:", site)
+                print("Invoice Price", invoice_price)
+                print("System Price", system_price)
+                print("Price from LPF:", price_from_lpf)
+                print("Document Reference:", document_reference)
+                print("Buyer", buyer)
 
-        else:
-            push_to_rejected_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer)
-            print("Invoice Rejected. Details:")
-            print("Invoice Date:", invoice_date)
-            print("Item Code:", item_code)
-            print("Site:", site)
-            print("Invoice Price", invoice_price)
-            print("System Price", system_price)
-            print("Document Reference:", document_reference)
-            print("Buyer", buyer)
+            else:
+                push_to_rejected_sheet(invoice_date, item_code, site, invoice_price, system_price, document_reference, buyer, price_from_lpf)
+                print("Invoice Rejected.")
+                print("Invoice Date:", invoice_date)
+                print("Item Code:", item_code)
+                print("Site:", site)
+                print("Invoice Price", invoice_price)
+                print("System Price", system_price)
+                print("Price from LPF:", price_from_lpf)
+                print("Document Reference:", document_reference)
+                print("Buyer", buyer)
+
+        continue_input = input("Do you want to continue? (yes/no): ")
+        if continue_input.lower() != 'yes':
+            break
 
 # Debugging code below
 
